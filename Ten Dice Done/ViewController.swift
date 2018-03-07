ed9ffe10d9737d3340ffdaf0d4b5025f356474b0
Michael Malmfeldt <dadfelt@gmail.com>
2018-03-07 18:35:59 -0500

    First commit. One ViewController with 10 dice and a few connections

---
+++ Ten Dice Done/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Ten Dice Done
+//
+//  Created by Michael Malmfeldt on 3/1/18.
+//  Copyright © 2018 Bad Ass Mother Fucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    var randomDiceIndex3 : Int = 0
+    var randomDiceIndex4 : Int = 0
+    var randomDiceIndex5 : Int = 0
+    var randomDiceIndex6 : Int = 0
+    var randomDiceIndex7 : Int = 0
+    var randomDiceIndex8 : Int = 0
+    var randomDiceIndex9 : Int = 0
+    var randomDiceIndex10 : Int = 0
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+    
+}
+
